bea66796d72dc76ea6c50d573abb0443cb38a569
Evgeny Savitsky <savickij.e@gmail.com>
2018-09-07 20:57:06 +0300

    openUrlQuit

--- src/test/java/seleniumjava/AppTest.java
+++ src/test/java/seleniumjava/AppTest.java
@@ -3,6 +3,7 @@ package seleniumjava;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -12,7 +13,9 @@ public class AppTest
 {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "chromedriver");
-        new ChromeDriver();
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://hillel.it/");
+        browser.quit();
     }
     }
   /*  @Test
